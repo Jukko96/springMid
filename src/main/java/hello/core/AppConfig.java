323c6998de57547c895567386aaab91bab1aa305
Jukko96 <cslzh4433@gmail.com>
2026-06-07 15:15:21 +0900

    config denpendency injection

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -12,6 +13,6 @@ public class AppConfig {
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl();
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
     }
 }
